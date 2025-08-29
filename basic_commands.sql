9ee9cb6c98ce0198ef8ac4bf03f03d32d072a5e4
Virgilio Espina <mygithub1974@gmail.com>
2025-08-29 10:37:48 +0800

    Update basic_commands.sql with latest changes

--- basic_commands.sql
+++ basic_commands.sql
@@ -93,3 +93,86 @@ INNER JOIN customers ON purchases.customer_id = customers.id
 GROUP BY customers.id;
 
 -- Order by / sorting data
+SELECT * FROM purchases INNER JOIN items on purchases.item_id = items.id;
+
+-- Total items the customers have spent in the business
+SELECT SUM(items.price) FROM purchases INNER JOIN items on purchases.item_id = items.id;
+
+-- List of items the customers have bought
+SELECT * FROM items
+INNER JOIN purchases ON purchases.item_id = items.id
+INNER JOIN customers ON purchases.customer_id = customers.id;
+
+-- How much each customer spent in the business in descending order
+SELECT customers.first_name, customers.last_name, SUM(items.price) AS "total_spent" FROM items
+INNER JOIN purchases ON purchases.item_id = items.id
+INNER JOIN customers ON purchases.customer_id = customers.id
+GROUP BY customers.id
+ORDER BY total_spent DESC;
+
+-- Top spender in the business
+SELECT customers.first_name, customers.last_name, SUM(items.price) AS "total_spent" FROM items
+INNER JOIN purchases ON purchases.item_id = items.id
+INNER JOIN customers ON purchases.customer_id = customers.id
+GROUP BY customers.id
+ORDER BY total_spent DESC
+LIMIT 1;
+
+-- CREATING A TABLE
+CREATE TABLE public.users (
+id integer PRIMARY KEY,
+name character varying(100) NOT NULL
+)
+
+CREATE TABLE public.videos (
+id integer PRIMARY KEY,
+user_id integer REFERENCES public.users,
+name character varying(255) NOT NULL
+)
+
+-- INSERTING INTO for adding data to a table
+INSERT INTO public.users(id, name)
+VALUES (1, 'marysmith');
+
+INSERT INTO public.users
+VALUES (2, 'rolfsmit');
+
+INSERT INTO public.videos
+VALUES (1, 2, 'Test video');
+
+SELECT * FROM public.videos 
+INNER JOIN public.users ON public.users.id = public.videos.user_id;
+
+-- SEQUENCE for auto-incrementing fields
+CREATE SEQUENCE users_id_seq START 3;
+
+ALTER TABLE public.users
+ALTER COLUMN id
+SET DEFAULT nextval('users_id_seq');
+
+ALTER SEQUENCE users_id_seq OWNED BY public.users.id;
+
+INSERT INTO public.users(name)
+VALUES ('josesalvatierra');
+
+SELECT * FROM public.users;
+
+-- CREATE INDEX and advanced information about indexes
+CREATE INDEX index_name ON table(column);
+CREATE INDEX index_name ON table(column_1, column_2);
+CREATE UNIQUE index index_name ON table(column)
+
+CREATE INDEX users_name_index ON public.users(name);
+SELECT * FROM public.users WHERE name = 'rolfsmit';
+
+REINDEX INDEX index_name;
+REINDEX TABLE index_name;
+REINDEX DATABASE index_name;
+
+-- DROP TABLE
+DROP TABLE public.users CASCADE;
+SELECT * FROM public.videos;
+
+DROP TABLE IF EXISTS public.videos;
+
+
